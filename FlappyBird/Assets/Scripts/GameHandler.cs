60af422c535d5320c4e10def285b11bf4a56bc8f
gladishd <gladish.dean@gmail.com>
2021-03-11 17:25:01 -0600

    CodeMonkey really has some great debugging tools! You can make alert messages follow wherever the mouse goes. Specifically, CMDebug.TextPopupMouse which I'm adding right now. So after downloading the game assets I have a bird, a background image, and some other textures, etc.  I have not added any scripts except for a temporary text popup on the GameHandler.cs component/script which is currently mounted on the Bird game object.

--- FlappyBird/Assets/Scripts/GameHandler.cs
+++ FlappyBird/Assets/Scripts/GameHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
 
 public class GameHandler : MonoBehaviour
 {
@@ -8,5 +10,11 @@ public class GameHandler : MonoBehaviour
     private void Start()
     {
         Debug.Log("GameHandler.Start");
+
+        int count = 0;
+        FunctionPeriodic.Create(() => {
+          CMDebug.TextPopupMouse("Ding! " + count);
+          count++;
+          }, .300f);
     }
 }
