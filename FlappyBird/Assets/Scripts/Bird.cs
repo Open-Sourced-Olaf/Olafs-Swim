2a3701e30e63d74bb99d73f76924d01c0357a8f6
gladishd <gladish.dean@gmail.com>
2021-03-11 18:06:55 -0600

    We can add a component to a collection of Objects!  For example, Box Collider 2D (on both PipeHead and PipeBody) as a trigger because otherwise, we won’t naturally invoke OnTriggerEnter2D in Bird.cs when one collider hits another collider (both the bird and the pipe are colliders).  So I should mention: previously I added Circle Collider 2D (and Rigidbody 2D) as components on the Bird game object, so now the bird just falls when you initiate the game by keypress space or left click.  Once the bird hits the pipe, OnTriggerEnter2D invokes CMDebug.TextPopupMouse which gives us some helpful placeholder text.  This log happens only once, because collision is a one-time thing.

--- FlappyBird/Assets/Scripts/Bird.cs
+++ FlappyBird/Assets/Scripts/Bird.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey;
 
 public class Bird : MonoBehaviour
 {
     private const float JUMP_AMOUNT = 100f;
+
     private Rigidbody2D birdRigidbody2D;
 
     private void Awake() {
       birdRigidbody2D = GetComponent<Rigidbody2D>();
     }
+
     private void Update() {
       if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
         Jump();
       }
     }
+
     private void Jump() {
       birdRigidbody2D.velocity = Vector2.up * JUMP_AMOUNT;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+      CMDebug.TextPopupMouse("Dead!");
     }
 }
